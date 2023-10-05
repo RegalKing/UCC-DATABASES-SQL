d5794625efe56e8ae24d8a5a518fe164ebc4839a
RegalKing <alexperkoplavec@gmail.com>
2023-10-05 12:24:12 +0100

    First lab exercises solved 24/26

---
+++ lab1/lab1.sql
@@ -0,0 +1,114 @@
+-- SELECT *
+-- FROM students; 
+-- 1. TASK
+
+-- SELECT first_name, last_name
+-- FROM students;
+-- 2. TASK
+
+-- SELECT first_name
+-- FROM students
+-- ORDER BY first_name ASC;
+-- 3. TASK
+
+-- SELECT first_name, date_of_birth
+-- FROM students
+-- ORDER BY date_of_birth ASC;
+-- 4. TASK
+
+-- SELECT DISTINCT hometown
+-- FROM students;
+-- 5. TASK
+
+-- SELECT *
+-- from students
+-- WHERE points>=450;
+-- 6. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE points=525;
+-- 7. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE points<>525;
+-- 8. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE points>400 AND points<500;
+-- 9. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE hometown='Cork';
+-- 10. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE date_of_birth<='1994-01-01';
+-- 11. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE date_of_birth>'1992-10-1';
+-- 12. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE date_of_birth='1994-12-25';
+-- 13. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE first_name='Ciara';
+-- 14. TASK
+
+
+-- SELECT *
+-- FROM students
+-- WHERE first_name='ciara';
+-- 15. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE first_name='Barry' OR last_name='Barry';
+-- 16. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE first_name='O\’Kelly' OR last_name='O\’Kelly';
+-- 17. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE date_of_birth>='1994-01-01' AND date_of_birth<='1994-12-31';
+-- 18. TASK
+
+-- 19. TASK -> CANT BE SOLVED WITH GIVEN DATA
+
+-- SELECT *
+-- FROM students
+-- WHERE course IN ('ck401', 'ck402');
+-- 20. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE points>=450 AND hometown='Cork';
+-- 21. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE points>=450 AND hometown<>'Cork';
+-- 22. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE last_name<'Cuddihy';
+-- 23. TASK
+
+-- SELECT *
+-- FROM students
+-- WHERE last_name<'Callaghan' AND first_name<'Harry';
+-- 23. TASK
